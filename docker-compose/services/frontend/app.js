e937393ae3c102c5a538a15f9b9c799de4186e53
Lee Warrick <leewarrick@gmail.com>
2022-07-18 11:46:56 -0400

    update compose file

--- docker-compose/services/frontend/app.js
+++ docker-compose/services/frontend/app.js
@@ -1,84 +1,80 @@
-const moneyBtn = document.getElementById('money');
-console.log(moneyBtn);
-const moreMoneyBtn = document.getElementById('moreMoney');
-const resultDiv = document.getElementById('result');
-const table = document.getElementById('resultTable');
-const tableBody = document.querySelector('tbody');
-const form = document.querySelector('form');
+const moneyBtn = document.getElementById("money");
+const moreMoneyBtn = document.getElementById("moreMoney");
+const resultDiv = document.getElementById("result");
+const table = document.getElementById("resultTable");
+const tableBody = document.querySelector("tbody");
+const form = document.querySelector("form");
 
 function getData() {
-    fetch('/api/coolpeeps')
-    .then(res => res.json())
-    .then(res => {
-        tableBody.innerHTML = "";
-        moreMoneyBtn.classList = "btn waves-effect purple darken-4";
-        res.forEach(row => {
-            const tr = document.createElement('tr');
-            const tdID = document.createElement('td');
-            const tdName = document.createElement('td');
-            const tdCoolness = document.createElement('td');
-            tdID.textContent = row.id;
-            tdName.textContent = row.name;
-            tdCoolness.textContent = row.coolness;
-            tr.appendChild(tdID);
-            tr.appendChild(tdName);
-            tr.appendChild(tdCoolness);
-            tableBody.appendChild(tr);
-        })
-        table.style.display = 'block';
+  fetch("/api/coolpeeps")
+    .then((res) => res.json())
+    .then((res) => {
+      tableBody.innerHTML = "";
+      moreMoneyBtn.classList = "btn waves-effect purple darken-4";
+      res.forEach((row) => {
+        const tr = document.createElement("tr");
+        const tdID = document.createElement("td");
+        const tdName = document.createElement("td");
+        const tdCoolness = document.createElement("td");
+        tdID.textContent = row.id;
+        tdName.textContent = row.name;
+        tdCoolness.textContent = row.coolness;
+        tr.appendChild(tdID);
+        tr.appendChild(tdName);
+        tr.appendChild(tdCoolness);
+        tableBody.appendChild(tr);
+      });
+      table.style.display = "block";
     })
-    .catch(err => {
-        console.log(err)
-        resultDiv.textContent = "Error: " + err.message + "😅";
+    .catch((err) => {
+      console.log(err);
+      resultDiv.textContent = "Error: " + err.message + "😅";
     });
 }
 
-moneyBtn.onclick = function(event) {
-    fetch('/api', {method: 'get', mode: "cors"})
-    .then(res => res.json())
-    .then(res => {
-        resultDiv.textContent = JSON.stringify(res) + " 🥳";
-        moneyBtn.style.display = 'none';
-        moreMoneyBtn.style.display = 'block';
+moneyBtn.onclick = function (event) {
+  fetch("/api", { method: "get", mode: "cors" })
+    .then((res) => res.json())
+    .then((res) => {
+      resultDiv.textContent = JSON.stringify(res) + " 🥳";
+      moneyBtn.style.display = "none";
+      moreMoneyBtn.style.display = "block";
     })
-    .catch(err => {
-        console.log(err)
-        resultDiv.textContent = "Error: " + err.message + "😅";
+    .catch((err) => {
+      console.log(err);
+      resultDiv.textContent = "Error: " + err.message + "😅";
     });
-}
-
-moreMoneyBtn.onclick = function() {
-    moreMoneyBtn.style.display = 'none';
-    getData();
-    form.style.display = 'block';
 };
 
-form.onsubmit = function(e) {
-    e.preventDefault();
-    const nameField = document.getElementById('coolName');
-    const coolnessField = document.getElementById('coolLevel');
-    const name = nameField.value;
-    const coolness = coolnessField.value;
-    nameField.value = '';
-    coolnessField.value= '';
-    // console.log({name, coolness});
-    fetch('/api/coolpeeps', {
-        method: 'POST',
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ name, coolness })
+moreMoneyBtn.onclick = function () {
+  moreMoneyBtn.style.display = "none";
+  getData();
+  form.style.display = "block";
+};
+
+form.onsubmit = function (e) {
+  e.preventDefault();
+  const nameField = document.getElementById("coolName");
+  const coolnessField = document.getElementById("coolLevel");
+  const name = nameField.value;
+  const coolness = coolnessField.value;
+  nameField.value = "";
+  coolnessField.value = "";
+  // console.log({name, coolness});
+  fetch("/api/coolpeeps", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ name, coolness }),
+  })
+    .then((res) => res.json())
+    .then((res) => {
+      console.log(res);
+      getData();
     })
-    .then(res => res.json())
-    .then(res => {
-        console.log(res);
-        getData();
-    })
-    .catch(err => {
-        console.log(err)
-        resultDiv.textContent = "Error: " + err.message + "😅";
+    .catch((err) => {
+      console.log(err);
+      resultDiv.textContent = "Error: " + err.message + "😅";
     });
-}
-
-
-
+};
--- docker-compose/services/node/index.js
+++ docker-compose/services/node/index.js
@@ -1,51 +1,59 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 const PORT = 3000;
-var mysql = require('mysql');
+var mysql = require("mysql");
 var connection = mysql.createConnection({
-  host     : 'mysql',
-  user     : 'test',
-  password : 'pass',
-  database : 'my_db'
+  host: "mysql",
+  user: "root",
+  database: "my_db",
 });
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
 });
 
-app.get('/api', function (req, res) {
-    res.json({ response: "hello!" });
+app.get("/api", function (req, res) {
+  res.json({ response: "hello!" });
 });
 
-app.get('/api/coolpeeps', function(req, res) {
-    connection.query('SELECT * FROM cool_people', function (error, results, fields) {
+app.get("/api/coolpeeps", function (req, res) {
+  connection.query("SELECT * FROM cool_people", function (
+    error,
+    results,
+    fields
+  ) {
+    if (error) throw error;
+    console.log(results);
+    res.json(results);
+  });
+});
+
+app.post("/api/coolpeeps", function (req, res) {
+  if (req.body.name && req.body.coolness) {
+    console.log(req.body);
+    connection.query(
+      "INSERT INTO cool_people SET ?",
+      {
+        name: req.body.name,
+        coolness: req.body.coolness,
+      },
+      function (error, results) {
         if (error) throw error;
-        console.log(results);
         res.json(results);
-    });
+      }
+    );
+  }
 });
-
-app.post('/api/coolpeeps', function(req, res) {
-    if (req.body.name && req.body.coolness) {
-        console.log(req.body);
-        connection.query('INSERT INTO cool_people SET ?', {
-            name: req.body.name,
-            coolness: req.body.coolness
-        },
-        function(error, results) {
-            if (error) throw error;
-            res.json(results);
-        });
-    }
-})
 
 app.listen(PORT, function () {
-    console.log("listening on http://localhost:" + PORT)
+  console.log("listening on http://localhost:" + PORT);
 });
-
